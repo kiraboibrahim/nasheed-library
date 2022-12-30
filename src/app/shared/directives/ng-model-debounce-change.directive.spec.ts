31d357d94624abcf587fe4687632437a1abb174a
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-12-30 15:16:03 +0300

    feat: Add a debouceChangeDirective for form controls

---
+++ src/app/shared/directives/ng-model-debounce-change.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { NgModelDebounceChangeDirective } from './ng-model-debounce-change.directive';
+
+describe('Directive: NgModelDebounceChange', () => {
+  it('should create an instance', () => {
+    const directive = new NgModelDebounceChangeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
