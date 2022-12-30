20f5e2e23ce49dfa1d5adfa46104e032c25e59e8
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-12-30 13:50:24 +0300

    fix: Initialize loading_tracks to false and only set it in search_tracks() method

--- src/app/modules/search/pages/search/search.component.ts
+++ src/app/modules/search/pages/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { MatTabChangeEvent } from '@angular/material/tabs';
@@ -7,7 +8,6 @@ import { Track } from 'src/app/data/types/track.model';
 import { Artist } from 'src/app/data/types/artist.model';
 import { EndOfPageScrollService } from 'src/app/core/services/end-of-page-scroll.service';
 import { MusicService } from 'src/app/data/service/music.service';
-import { isThisTypeNode } from 'typescript';
 
 
 @Component({
@@ -15,9 +15,9 @@ import { isThisTypeNode } from 'typescript';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
   tracks: Track[] = [];
-  loading_tracks: boolean = true;
+  loading_tracks: boolean = false;
   tracks_page_number: number = 1;
   reached_last_page_of_tracks_results: boolean = false;
 
@@ -26,6 +26,7 @@ export class SearchComponent implements OnInit {
   artists_page_number: number = 1;
   reached_last_page_of_artists_results: boolean = false;
 
+  end_of_page_scroll_subscription: Subscription;
   query: string | null;
   search_context: string = "tracks";
 
@@ -38,31 +39,30 @@ export class SearchComponent implements OnInit {
   ngOnInit(): void {
     this.activated_route.paramMap.subscribe((params: ParamMap) => {
       this.query = params.get("query");
-      // The first time this route is loaded, the tracks tab is already active and the loading_tracks
-      // property is true and yet fetching of tracks is prohibited when there is an ongoing
-      // fetch.(Multiple subscriptions). The search method takes an initial_search argument to
-      // inform it if it is the time when the page is loaded to allow fetching of tracks
-      this.search(true);
+      this.search();
     });
-    this.end_of_page_scroll_service.end_of_page_scroll$.subscribe((event: Event) => {
+    this.end_of_page_scroll_subscription = this.end_of_page_scroll_service.end_of_page_scroll$.subscribe((event: Event) => {
       this.search();
     });
   }
 
   on_submit(form: NgForm) {
-    this.router.navigate(["search", form.value.query]);
-    this.reset_search();
-    this.search();
+    this.router.navigate(["search", form.value.query]).then((navigated: boolean | null) => {
+      if(navigated == true) {
+        this.reset_search();
+        this.search();
+      }
+    });
   }
 
   reset_search(): void {
     this.tracks = [];
-    this.loading_tracks = this.search_context == "tracks" ? true : false;
+    this.loading_tracks = false;
     this.tracks_page_number = 1;
     this.reached_last_page_of_tracks_results = false;
 
     this.artists = [];
-    this.loading_artists = this.search_context == "artists" ? true : false;
+    this.loading_artists = false;
     this.artists_page_number = 1;
     this.reached_last_page_of_artists_results = false;
   }
@@ -79,24 +79,20 @@ export class SearchComponent implements OnInit {
     if(this.tracks.length == 0 || this.artists.length == 0) this.search();
   }
 
-  search(initial_search: boolean = false): void {
-    if(this.query) {
-      switch(this.search_context) {
-        case "artists":
-          this.search_artists(initial_search);
-          break;
-        case "tracks":
-          this.search_tracks(initial_search);
-          break;
-      }
-    } else {
-      this.loading_artists = false;
-      this.loading_tracks = false;
+  search(): void {
+    if(!this.query) return;
+    switch(this.search_context) {
+      case "artists":
+        this.search_artists();
+        break;
+      case "tracks":
+        this.search_tracks();
+        break;
     }
   }
 
-  search_artists(initial_search: boolean = false) {
-    if(!this.reached_last_page_of_artists_results && (!this.loading_artists || initial_search)) {
+  search_artists() {
+    if(!this.reached_last_page_of_artists_results && !this.loading_artists) {
       this.loading_artists = true;
       this.music_service.search_artists(<string>this.query, this.artists_page_number).
       subscribe((data: any) => {
@@ -108,8 +104,8 @@ export class SearchComponent implements OnInit {
     }
   }
 
-  search_tracks(initial_search: boolean = false) {
-    if(!this.reached_last_page_of_tracks_results && (!this.loading_tracks || initial_search)) {
+  search_tracks() {
+    if(!this.reached_last_page_of_tracks_results && !this.loading_tracks) {
       this.loading_tracks = true;
       this.music_service.search_tracks(<string>this.query, this.tracks_page_number).
       subscribe((data: any) => {
@@ -121,5 +117,9 @@ export class SearchComponent implements OnInit {
     } else {
       console.log(this.reached_last_page_of_tracks_results, this.loading_tracks);
     }
+  }
+
+  ngOnDestroy(): void {
+    if(this.end_of_page_scroll_subscription) this.end_of_page_scroll_subscription.unsubscribe();
   }
 }
