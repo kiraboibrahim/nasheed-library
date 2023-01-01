c5af9fd773ff832d34fae4d029d6abd486beebce
Kirabo Ibrahim <kiraboibra268@gmail.com>
2023-01-01 14:10:55 +0300

    refactor: Remove explicit delays that were added to the MusicService simulate real network latency

--- src/app/data/service/music.service.ts
+++ src/app/data/service/music.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { delay, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { Artist } from '../types/artist.model';
@@ -22,26 +22,26 @@ export class MusicService {
 
   get_artist_tracks(artist: Artist, page_number: number = 1): Observable<any> {
     let url: string = `${this.api_base_url}/artists/${artist.id}/tracks?page=${page_number}`;
-    return this.http.get(url).pipe(delay(3000));
+    return this.http.get(url);
   }
 
   get_artists(page_number: number): Observable<any> {
     let url = `${this.api_base_url}/artists?page=${page_number}`;
-    return this.http.get(url).pipe(delay(3000));
+    return this.http.get(url);
   }
 
   get_tracks(page_number: number): Observable<any> {
     let url = `${this.api_base_url}/tracks?page=${page_number}`;
-    return this.http.get(url).pipe(delay(3000));
+    return this.http.get(url);
   }
 
   search_tracks(query: string, page_number: number) {
     let url = `${this.api_base_url}/tracks/search/${query}?page=${page_number}`;
-    return this.http.get(url).pipe(delay(3000));
+    return this.http.get(url);
   }
 
   search_artists(query: string, page_number: number) {
     let url = `${this.api_base_url}/artists/search/${query}?page=${page_number}`;
-    return this.http.get(url).pipe(delay(3000));
+    return this.http.get(url);
   }
 }
